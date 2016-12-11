dc6066307b5ebbd6614e993d716236adac5e6c58
Gopal <gopalsohun@gmail.com>
2016-12-12 01:59:32 +0400

    book search with google api

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 from googleapiclient.discovery import build
 from kivy.app import App
 from kivy.network.urlrequest import UrlRequest
@@ -7,23 +8,54 @@ import json
 
 
 class AddBookForm(BoxLayout):
+    googleapikey = "AIzaSyDgEcXMWxJo7BMfkjHRArzvZY9-5Vw3iTk"
     search_input = ObjectProperty()
 
     def search_book(self):
         print("The user searched for '{}'".format(self.search_input.text))
 
     def search_book(self):
-        books_service = build('books', 'v1', developerKey='AIzaSyDgEcXMWxJo7BMfkjHRArzvZY9-5Vw3iTk')
-        search_template = "https://www.googleapis.com/books/v1/volumes?q="
-        search_url = search_template.format(self.search_input.text)
-        request = UrlRequest(search_url, self.found_book)
+        parms = {"q":self.search_input.text, 'key':self.googleapikey}
+        print("here")
+        search_template = "https://www.googleapis.com/books/v1/volumes"
 
-    def found_book(self, request, data):
-        data = json.loads(data.decode()) if not isinstance(data, dict) else data
-        books = ["{}({})".format(d['title'], d['authors']['categories'])
-            for d in data['list']]
+        search_url = requests.get(url=search_template, params=parms)
+
+        print(search_url.url)
+
+        data = search_url.json()
+        # print(data["totalItems"])
+
+        #
+        # for d in data["items"]:
+        #     try:
+        #         # print(repr(i["volumeInfo"]["description"]))
+        #         print("".join(d["volumeInfo"]["title"]))
+        #         self.search_results.item_strings =
+        #     except:
+        #         pass
+            # try:
+            #     # print(repr((i["volumeInfo"]["imageLinks"]["thumbnail"])))
+            #     self.search_results.item_strings = i["volumeInfo"]["authors"]
+            # except:
+            #     pass
+
+        books = ["{} by {}".format(d["volumeInfo"]["title"], d["volumeInfo"]["authors"][0])
+            for d in data["items"]]
         print("\n".join(books))
         self.search_results.item_strings = books
+
+
+        # search_url = search_template.format(self.search_input.text)
+        # request = UrlRequest(search_url, self.found_book)
+
+    def found_book(self, request, data):
+        data = json.loads(data.decode()) if not isinstance(data, dict) else data #turn into a dictionary
+        books = ["{}{}".format(d['title'], d['authors'])
+                 for d in data['items']]
+        print("\n".join(books))
+        self.search_results.item_strings = books
+
 
 class BooksRecommendationApp(App):
     pass
