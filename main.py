1109c90bf3f56ae602a1d30c8f6c38401033bf0b
Gopal <gopalsohun@gmail.com>
2016-12-11 22:21:46 +0400

    integrated google books v1 api

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from googleapiclient.discovery import build
 from kivy.app import App
+from kivy.network.urlrequest import UrlRequest
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
+import json
 
 
 class AddBookForm(BoxLayout):
@@ -9,6 +12,18 @@ class AddBookForm(BoxLayout):
     def search_book(self):
         print("The user searched for '{}'".format(self.search_input.text))
 
+    def search_location(self):
+        search_template = "https://www.googleapis.com/books/v1/volumes?q="
+        search_url = search_template.format(self.search_input.text)
+        request = UrlRequest(search_url, self.found_book)
+
+    def found_book(self, request, data):
+        books_service = build('books', 'v1', developerKey='AIzaSyDgEcXMWxJo7BMfkjHRArzvZY9-5Vw3iTk')
+        data = json.loads(data.decode()) if not isinstance(data, dict) else data
+        books = ["{}({})".format(d['title'], d['authors']['categories'])
+            for d in data['list']]
+        print("\n".join(books))
+        self.search_results.item_strings = books
 
 class BooksRecommendationApp(App):
     pass
