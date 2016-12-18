ce2ee2dc4fa672d4a739e40df51ad350f4465be2
Chitra Nundlall <shwetanundlall@gmail.com>
2016-12-18 14:33:36 +0400

    Testing

---
+++ login/Registermain.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class AddRegisterForm(BoxLayout):
+    pass
+
+class LoginApp(App):
+    pass
+
+if __name__ == '__main__':
+    LoginApp().run()
