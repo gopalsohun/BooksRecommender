a4d357fe014f0f46a237b628d83fc45899716327
Chitra Nundlall <shwetanundlall@gmail.com>
2016-12-23 13:25:57 +0400

    work

--- login/loginmain.py
+++ login/loginmain.py
@@ -1,12 +1,18 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-
+from kivy.properties import ObjectProperty
 
 class AddLoginForm(BoxLayout):
+
+    UserText = ObjectProperty()
+    Password = ObjectProperty()
+
     def do_login(self):
-        self.login.text= " Text data " + self.UserTextField.text
+        print(self.UserText.text)
+        print(self.Password.text)
 
 class LoginApp(App):
+
     def build(self):
         return AddLoginForm()
 
