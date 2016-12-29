8a6cacacf11f7c4ab96c49b6ba276ba4292155e0
Chitra Nundlall <shwetanundlall@gmail.com>
2016-12-29 20:12:52 +0400

    work

--- SignInUp/signinmain.py
+++ SignInUp/signinmain.py
@@ -1,15 +1,31 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.properties import ObjectProperty  # at top of file
+import sqlite3
 
 class SignInForm(AnchorLayout):
     username_box = ObjectProperty()
     password_box = ObjectProperty()
 
     def login(self):
-        print(self.username_box.text)
-        print(self.password_box.text)
+        conn = sqlite3.connect('booksrecommender.db')
+        password = self.password_box.text
+        username = self.username_box.text
+        cursor = conn.cursor()
+        for row in username:
+            cursor.execute("SELECT UserID FROM Authentication WHERE UserName = ?", (username,))
+            data = cursor.fetchall()
+        if len(data) == 0:
+            print("No username available you need to sign up!")
+        else:
+            for row in password:
+                cursor.execute("SELECT UserID FROM Authentication WHERE Password = ?", (password,))
+                data = cursor.fetchall()
+            if len(data) == 0:
+                print("Wrong password. Try again!")
+            else:
+                print("Welcome")
+
 
 class SignIn(App):
     def build(self):
