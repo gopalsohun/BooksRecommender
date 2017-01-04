7be3b1045c7ae283265e47a93e2e8ed943a4651d
Chitra Nundlall <shwetanundlall@gmail.com>
2017-01-04 13:02:46 +0400

    work

--- SignInUp/signinmain.py
+++ SignInUp/signinmain.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 from kivy.properties import ObjectProperty  # at top of file
 import sqlite3
 
@@ -17,14 +19,29 @@ class SignInForm(AnchorLayout):
             data = cursor.fetchall()
         if len(data) == 0:
             print("No username available you need to sign up!")
+            popup = Popup(title="Information", content=Label(text="No username available you need to sign up!"),
+                          size_hint=(None, None),
+                          size=(400, 200))
+            popup.open()
+            return
+
         else:
             for row in password:
                 cursor.execute("SELECT UserID FROM Authentication WHERE Password = ?", (password,))
                 data = cursor.fetchall()
             if len(data) == 0:
                 print("Wrong password. Try again!")
+                popup = Popup(title="Information", content=Label(text="Wrong password. Try again!"), size_hint=(None, None),
+                              size=(400, 200))
+                popup.open()
+                return
             else:
                 print("Welcome")
+                popup = Popup(title="Information", content=Label(text="Welcome"),
+                              size_hint=(None, None),
+                              size=(400, 200))
+                popup.open()
+                return
 
 class SignIn(App):
     def build(self):
